59ceb973b969f2bb7b7630465125099da1e328de
Amjadbakir <62174830+Amjadbakir@users.noreply.github.com>
2025-04-06 13:16:07 +0200

    Added simple selection color

--- src/selection.ts
+++ src/selection.ts
@@ -3,14 +3,14 @@ import * as THREE from 'three';
 export let selectedMesh: THREE.Mesh | null = null;
 
 export function setupModelSelection(
-  scene: THREE.Scene,
-  camera: THREE.Camera,
-  domElement: HTMLElement
-) {
-  const raycaster = new THREE.Raycaster();
-  const mouse = new THREE.Vector2();
+    scene: THREE.Scene,
+    camera: THREE.Camera,
+    domElement: HTMLElement
+    ) {
+    const raycaster = new THREE.Raycaster();
+    const mouse = new THREE.Vector2();
 
-  domElement.addEventListener('click', (event: MouseEvent) => {
+    domElement.addEventListener('click', (event: MouseEvent) => {
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
@@ -18,14 +18,16 @@ export function setupModelSelection(
     const intersects = raycaster.intersectObjects(scene.children, true);
 
     if (intersects.length > 0) {
-      const selectedObject = intersects[0].object;
+        const selectedObject = intersects[0].object;
 
-      selectedMesh = selectedObject as THREE.Mesh;
-      extend();
+        selectedMesh = selectedObject as THREE.Mesh;
+        selectedMesh.material.emissive.set(0x3333ff);
+        extend();
     } else {
-      selectedMesh = null;
+        selectedMesh.material.emissive.set(0x000000);
+        selectedMesh = null;
     }
-  });
+    });
 }
 
 function extend(): void {
